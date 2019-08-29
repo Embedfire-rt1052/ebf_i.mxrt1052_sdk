394f2791d45244c5b3774be957945123a7a28d27
tinypanda <1656125469@qq.com>
2019-08-29 10:53:51 +0800

    LED 闪烁程序能够运行，debug 版本可以，release 只有一个版本可以，其他两个提示找不到下载算法，

--- SDK_2.6.1_MIMXRT1052xxxxB/boards/evkbimxrt1050/xip/evkbimxrt1050_flexspi_nor_config.c
+++ SDK_2.6.1_MIMXRT1052xxxxB/boards/evkbimxrt1050/xip/evkbimxrt1050_flexspi_nor_config.c
@@ -22,34 +22,87 @@ __attribute__((section(".boot_hdr.conf")))
 #pragma location = ".boot_hdr.conf"
 #endif
 
-const flexspi_nor_config_t hyperflash_config = {
+const flexspi_nor_config_t spiflash_config = {
     .memConfig =
         {
-            .tag                = FLEXSPI_CFG_BLK_TAG,
-            .version            = FLEXSPI_CFG_BLK_VERSION,
-            .readSampleClkSrc   = kFlexSPIReadSampleClk_ExternalInputFromDqsPad,
-            .csHoldTime         = 3u,
-            .csSetupTime        = 3u,
-            .columnAddressWidth = 3u,
-            // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
-            .controllerMiscOption =
-                (1u << kFlexSpiMiscOffset_DdrModeEnable) | (1u << kFlexSpiMiscOffset_WordAddressableEnable) |
-                (1u << kFlexSpiMiscOffset_SafeConfigFreqEnable) | (1u << kFlexSpiMiscOffset_DiffClkEnable),
-            .sflashPadType = kSerialFlash_8Pads,
-            .serialClkFreq = kFlexSpiSerialClk_133MHz,
-            .sflashA1Size  = 64u * 1024u * 1024u,
-            .dataValidTime = {16u, 16u},
+            .tag = FLEXSPI_CFG_BLK_TAG,/*标志：FCFB*/
+            .version = FLEXSPI_CFG_BLK_VERSION,/*版本：V1.4.0*/
+            .readSampleClkSrc = kFlexSPIReadSampleClk_LoopbackInternally,/*内部环回*/
+            .csHoldTime = 3u, /*保持时间*/
+            .csSetupTime = 3u,/*建立时间*/
+            .columnAddressWidth = 0u,/*列地址宽度*/
+            .deviceModeCfgEnable = 1u,/*设备模式配置使能*/
+            .deviceModeType = 1u,/*Quad 使能命令*/
+            .deviceModeSeq.seqNum = 1u,/*LUT序列号*/
+            .deviceModeSeq.seqId = 4u, /*LUT序列索引*/    
+            .deviceModeArg = 0x000200,/*设置 QE=1（S9）*/
+            .deviceType = kFlexSpiDeviceType_SerialNOR,/*设备类型为nor flash*/
+            .sflashPadType = kSerialFlash_4Pads,/*设备数据总线为4*/
+            .serialClkFreq = kFlexSpiSerialClk_133MHz,/*flash 时钟*/
+            .sflashA1Size = 32u * 1024u * 1024u,      /*flash 大小32MBytes*/
+            //.dataValidTime = {16u, 16u},
             .lookupTable =
                 {
-                    // Read LUTs
-                    FLEXSPI_LUT_SEQ(CMD_DDR, FLEXSPI_8PAD, 0xA0, RADDR_DDR, FLEXSPI_8PAD, 0x18),
-                    FLEXSPI_LUT_SEQ(CADDR_DDR, FLEXSPI_8PAD, 0x10, DUMMY_DDR, FLEXSPI_8PAD, 0x06),
-                    FLEXSPI_LUT_SEQ(READ_DDR, FLEXSPI_8PAD, 0x04, STOP, FLEXSPI_1PAD, 0x0),
+                    /*快速读命令（四线）*/
+                    [0]     = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xEC, RADDR_SDR, FLEXSPI_4PAD, 0x20),
+                    [1]     = FLEXSPI_LUT_SEQ(DUMMY_SDR, FLEXSPI_4PAD, 0x06, READ_SDR, FLEXSPI_4PAD, 0x04),
+                    
+
+                    /*读状态命令*/
+                    [1*4]   = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x05, READ_SDR, FLEXSPI_1PAD, 0x04),
+                    /*写使能命令*/
+                    [3*4]   = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x06, STOP, FLEXSPI_1PAD, 0),      
+                    /*擦除扇区命令*/
+                    [5*4]   = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x20, RADDR_SDR, FLEXSPI_1PAD, 0x04),
+                    /*页编程命令（四线）*/
+                    [9*4]   = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0x32, RADDR_SDR, FLEXSPI_1PAD, 0x18),  
+                    [9*4+1] = FLEXSPI_LUT_SEQ(WRITE_SDR,FLEXSPI_4PAD , 0x04, STOP, FLEXSPI_1PAD, 0),                  
+                    /*整片擦除*/
+                    [11*4]  = FLEXSPI_LUT_SEQ(CMD_SDR, FLEXSPI_1PAD, 0xc7, STOP, FLEXSPI_1PAD, 0),                  
                 },
         },
-    .pageSize           = 512u,
-    .sectorSize         = 256u * 1024u,
-    .blockSize          = 256u * 1024u,
-    .isUniformBlockSize = true,
+    .pageSize = 256u,/*页大小为256字节*/
+    .sectorSize = 4u * 1024u,/*扇区大小为4k字节*/
 };
+
+
+
+
+
+
+
+
+
+
+
+//const flexspi_nor_config_t hyperflash_config = {
+//    .memConfig =
+//        {
+//            .tag                = FLEXSPI_CFG_BLK_TAG,
+//            .version            = FLEXSPI_CFG_BLK_VERSION,
+//            .readSampleClkSrc   = kFlexSPIReadSampleClk_ExternalInputFromDqsPad,
+//            .csHoldTime         = 3u,
+//            .csSetupTime        = 3u,
+//            .columnAddressWidth = 3u,
+//            // Enable DDR mode, Wordaddassable, Safe configuration, Differential clock
+//            .controllerMiscOption =
+//                (1u << kFlexSpiMiscOffset_DdrModeEnable) | (1u << kFlexSpiMiscOffset_WordAddressableEnable) |
+//                (1u << kFlexSpiMiscOffset_SafeConfigFreqEnable) | (1u << kFlexSpiMiscOffset_DiffClkEnable),
+//            .sflashPadType = kSerialFlash_8Pads,
+//            .serialClkFreq = kFlexSpiSerialClk_133MHz,
+//            .sflashA1Size  = 64u * 1024u * 1024u,
+//            .dataValidTime = {16u, 16u},
+//            .lookupTable =
+//                {
+//                    // Read LUTs
+//                    FLEXSPI_LUT_SEQ(CMD_DDR, FLEXSPI_8PAD, 0xA0, RADDR_DDR, FLEXSPI_8PAD, 0x18),
+//                    FLEXSPI_LUT_SEQ(CADDR_DDR, FLEXSPI_8PAD, 0x10, DUMMY_DDR, FLEXSPI_8PAD, 0x06),
+//                    FLEXSPI_LUT_SEQ(READ_DDR, FLEXSPI_8PAD, 0x04, STOP, FLEXSPI_1PAD, 0x0),
+//                },
+//        },
+//    .pageSize           = 512u,
+//    .sectorSize         = 256u * 1024u,
+//    .blockSize          = 256u * 1024u,
+//    .isUniformBlockSize = true,
+//};
 #endif /* XIP_BOOT_HEADER_ENABLE */
